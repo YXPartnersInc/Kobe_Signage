4900905b6d3f6aaa39a903a3f774a13373039f92
koyochan <121277745+koyochan@users.noreply.github.com>
2025-02-09 16:23:36 +0900

    Vercelにデプロイできるように設定ファイルを書き換えました

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
+
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react()],
+
+  adapter: vercel(),
 })
